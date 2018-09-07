b49494acea7991af135fea9b0d5ed9bd874f36f7
TANNO Mahito <pb94.mahito@gmail.com>
2018-09-07 23:20:23 +0900

    setting for SINGULAR

---
+++ conf/22_exec-path-from-shell.el
@@ -0,0 +1,2 @@
+;; path
+(exec-path-from-shell-initialize)
---
+++ custom-file.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (exec-path-from-shell use-package rainbow-delimiters powerline package-utils init-loader hiwin helm dashboard dash auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
