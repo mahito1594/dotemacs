0fd3dc9488080e916a908a0da7a9963899a6bbe3
TANNO Mahito <pb94.mahito@gmail.com>
2018-09-11 21:00:09 +0900

    key bind: C-h <=> backspace

--- conf/99_keybind.el
+++ conf/99_keybind.el
@@ -2,6 +2,11 @@
 (define-key global-map (kbd "C-2") 'set-mark-command)
 (define-key global-map (kbd "C-t") 'other-window)
 (define-key global-map (kbd "C-x w") 'whitespace-mode)
-(define-key key-translation-map [?\C-h] [?\C-?])
+(define-key global-map (kbd "C-c t") 'toggle-truncate-lines)
+(define-key global-map (kbd "C-;") 'comment-line)
+
+;; C-h => backspace, backspace => help
+(define-key key-translation-map (kbd "C-h") (kbd "DEL"))
+(define-key key-translation-map (kbd "DEL") (kbd "C-h"))
 
 (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
