c2148fcd43d63c0c4d46a8e8367c8d3ac6f64064
TANNO Mahito <pb94.mahito@gmail.com>
2019-02-10 01:21:40 +0900

    emacs 標準のコマンドのいくつかをHelmのものを使うようにした

--- conf/10_helm.el
+++ conf/10_helm.el
@@ -1,10 +1,39 @@
-;;; helm
+;;; helm  --- settings for helm
+;;; Commentary:
+;; Replace some build-in commands with Helm's.
+;;
+;;; Code:
 
 (use-package helm
+  :bind (("M-x" . helm-M-x)
+         ("M-y" . helm-show-kill-ring)
+         ("C-x C-f" . helm-find-files)
+         ("C-x b" . helm-mini)
+         :map helm-map
+         ("<tab>" . helm-execute-persistent-action)
+         ("C-i" . helm-execute-persistent-action)
+         ("C-z" . helm-select-action))
   :config
   (use-package helm-config
     :ensure nil)
-  (helm-mode 1))
+  (helm-mode 1)
+  )
 
-(provide '20_helm)
-;;; 20_helm.el ends here
+(use-package helm-swoop
+  :after (helm)
+  :bind (("M-i" . helm-swoop)
+         ("M-I" . helm-swoop-back-to-last-point)
+         ("C-c M-i" . helm-multi-swoop)
+         ("C-x M-i" . helm-multi-swoop-all)
+         :map isearch-mode-map
+         ("M-i" . helm-swoop-from-isearch)
+         :map helm-swoop-map
+         ("C-r" . helm-previous-line)
+         ("C-s" . helm-next-line)
+         :map helm-multi-swoop-map
+         ("C-r" . helm-previous-line)
+         ("C-s" . helm-next-line))
+  )
+
+(provide '10_helm)
+;;; 10_helm.el ends here
